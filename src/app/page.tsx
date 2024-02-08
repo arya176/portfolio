47866f7a3834a0c8fdf1f70748ec6df060c3ab68
arya <aryaradfar@gmail.com>
2024-02-09 01:58:48 +1100

    fix images  link and finalize the desiugn concept

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import Image from "next/image";
-import bgImg from "/Users/aryamacbook/Desktop/next/portfolio/public/images/234360563223.png";
-import bgImg2 from "/Users/aryamacbook/Desktop/next/portfolio/public/images/www.png";
-import bgImg3 from "/Users/aryamacbook/Desktop/next/portfolio/public/images/hand.png";
-import bgImg4 from "/Users/aryamacbook/Desktop/next/portfolio/public/images/light.png";
+import bgImg from "/public/images/234360563223.png";
+import bgImg2 from "/public/images/www.png";
+import bgImg3 from "/public/images/hand.png";
+import bgImg4 from "/public/images/light.png";
 
 export default function Home() {
   // useEffect(() => {
@@ -22,21 +22,24 @@ export default function Home() {
   return (
     <main className="flex  flex-col flex-wrap items-center bg-yellow-100 text-neutral-950 font-bold m-0 p-0 ">
       <div className="">
-        <nav className="bg-yellow-100 flex flex-row justify-items-end  m-0 pt-0 pl-3 w-[100%]  pb-10 text-[#11A068] ">
-          <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#CDF0EA]">
-            HOME
-          </div>
-          <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#FDE2E2]">
-            DESIGNS
-          </div>
-          <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950  mr-2 bg-[#F9F5E7]">
-            MY UX/UI
-          </div>
-          <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#CAF7E3]">
-            ABOUT
-          </div>
-          <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#E1F0DA]">
-            MY LOCAL TIME
+        <nav className="bg-yellow-100 flex flex-row   m-0 pt-0 pl-3 w-[100%]  pb-10 text-[#11A068] ">
+          <div className="flex flex-row flex-wrap m-auto">
+            <div className="p-5 border text-wrap border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#D9EDBF] hover:bg-[#11A068] hover:text-[#FFD966] hover:pt-14">
+              HOME
+            </div>
+            <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#FFB996] hover:bg-[#11A068] hover:text-[#FFD966]">
+              MY LOCAL TIME
+            </div>
+
+            <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#FFB996] hover:bg-[#11A068] hover:text-[#FFD966]">
+              DESIGNS
+            </div>
+            <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950  mr-2 bg-[#FFB996] hover:bg-[#11A068] hover:text-[#FFD966]">
+              MY UX/UI
+            </div>
+            <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 bg-[#FFB996] hover:bg-[#11A068] hover:text-[#FFD966]">
+              ABOUT
+            </div>
           </div>
 
           <Image
@@ -48,16 +51,7 @@ export default function Home() {
           />
         </nav>
 
-        <h1 className="pt-16 pl-[40%] pb-7 font-sans font-extrabold text-[#0A1D37] text-[4rem] text-left">
-          I am <span className="text-[#FF597B] font-mono">Arya</span>
-        </h1>
-        <div id="h2" className="p-0 m-0  ">
-          <h2 className="pt-3 mt-0 pl-[30%] ml-72 font-mono font-extrabold text-[#0A1D37] text-4xl flex rotate-[12deg] ">
-            A <span className="text-[#FF597B]">Future </span>Web-Developer
-          </h2>
-        </div>
-
-        <div className="flex flex-row mt-0 ">
+        <div className="flex flex-row mt-0 pt-[10rem] ">
           <div className="">
             <Image
               className=""
@@ -67,7 +61,7 @@ export default function Home() {
               height={700}
             />
           </div>
-          <h2 className="w-1/2  text-[#5F8670] m-0 p-0 text-[3.5rem] self-center pt-24 font-sans ">
+          <h2 className="w-1/2  text-[#5F8670] m-0 p-0 text-[3rem] self-center pt-24 font-sans ">
             <q>
               Give someone a program; you frustrate them for a day; teach them
               how to program, and you frustrate them for a lifetime
@@ -78,7 +72,7 @@ export default function Home() {
         </div>
       </div>
       <hr className=" w-[60%] h-2 border-solid border-stone-950 mb-20 self-start" />
-      <div className="flex flex-row bg-yellow-100] pb-48">
+      <div className="flex flex-row bg-yellow-100] pb-24">
         <div className="font-bold mb-24  text-3xl self-center pt-40  bg-yellow-100   ">
           <ul className="list-disc text-pretty  text-[#525E75] font-thin  ">
             <li>Certificated in 3D studio Max</li>
@@ -104,38 +98,42 @@ export default function Home() {
         </div>
       </div>
       <div className="align-center text-center text-4xl text-[#11A068] font-extrabold pb-2">
-        DO YOU KNOW ME?
+        NICE TO MEET YOU ...
       </div>
+      <h1 className="pt-8 pb-7 text-[#080202] text-[2rem] text-right font-sans font-light">
+        I am <span className="text-[#FF8080] font-mono">Arya</span>, A
+        <span className="text-[#FF8080] font-mono"> Future</span> Web-Developer
+      </h1>
       <div className="flex flex-row ">
         <div className="flex flex-col flex-wrap  text-center ">
-          <div className="border  border-teal-950 p-6 mt-44 rounded-lg ">
+          <div className="border  border-teal-950 p-6 mt-44 rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I Love Dogs
           </div>
-          <div className="border  border-teal-950 p-6   rounded-lg">
+          <div className="border  border-teal-950 p-6   rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I Love Trade
           </div>
-          <div className="border  border-teal-950 p-6  rounded-lg">
+          <div className="border  border-teal-950 p-6  rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I am intrested in Photography
           </div>
-          <div className="border  border-teal-950 p-6   rounded-lg">
+          <div className="border  border-teal-950 p-6   rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I am intrested in Drawing & painting
           </div>
-          <div className="border  border-teal-950 rounded-lg">
+          <div className="border  border-teal-950 p-6  rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I am intrested in Computer Gaming
           </div>
-          <div className="border  border-teal-950 p-6   rounded-lg">
+          <div className="border  border-teal-950 p-6   rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I am intrested in Metaphysics
           </div>
-          <div className="border  border-teal-950 p-6   rounded-lg">
+          <div className="border  border-teal-950 p-6   rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I am intrested in History
           </div>
-          <div className="border  border-teal-950 p-6  rounded-lg">
+          <div className="border  border-teal-950 p-6  rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I am intrested in Politics
           </div>
-          <div className="border  border-teal-950 p-6   rounded-lg">
+          <div className="border  border-teal-950 p-6   rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I love Design
           </div>
-          <div className="border  border-teal-950 p-6   rounded-lg">
+          <div className="border  border-teal-950 p-6   rounded-lg text-[#F0A500] hover:shadow-lg shadow-[#F0A500] hover:bg-[#D9EDBF]">
             I Love My Friends
           </div>
         </div>
@@ -151,7 +149,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="mt-40 bg-[#14A369] w-[100%] pt-36 pb-8 ">
+      <div className="mt-28 bg-[#14A369] w-[100%] pt-36 pb-8 ">
         <footer id="footer" className="font-normal">
           <section className="text-center">
             <a href="https://www.facebook.com/arya.messenger" target="_self">
