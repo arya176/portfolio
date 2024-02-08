3a01768b3e232c7791de0af8a9a5c61165d6438d
arya <aryaradfar@gmail.com>
2024-02-08 12:36:02 +1100

    fix h2 shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import bgImg from "/Users/aryamacbook/Desktop/next/portfolio/public/images/234360563223.png";
 import bgImg2 from "/Users/aryamacbook/Desktop/next/portfolio/public/images/www.png";
 import bgImg3 from "/Users/aryamacbook/Desktop/next/portfolio/public/images/hand.png";
+import bgImg4 from "/Users/aryamacbook/Desktop/next/portfolio/public/images/light.png";
 
 export default function Home() {
   // useEffect(() => {
@@ -19,7 +20,7 @@ export default function Home() {
   //   heading.style.transform = `rotate(${32}deg)`;
   // };
   return (
-    <main className="flex min-h-screen flex-col items-center bg-yellow-100 text-neutral-950 font-bold m-0 p-0 ">
+    <main className="flex  flex-col flex-wrap items-center bg-yellow-100 text-neutral-950 font-bold m-0 p-0 ">
       <div className="">
         <nav className="bg-yellow-100 flex flex-row justify-items-end  m-0 pt-0 pl-3 w-[100%]  pb-10 text-[#11A068] ">
           <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#CDF0EA]">
@@ -28,23 +29,35 @@ export default function Home() {
           <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#FDE2E2]">
             DESIGNS
           </div>
+          <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950  mr-2 bg-[#F9F5E7]">
+            MY Photography
+          </div>
           <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#CAF7E3]">
             ABOUT
           </div>
-          <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 bg-[#F9F5E7]">
-            MY TIME
+          <div className="p-5 border border-t-0 rounded-br-3xl rounded-bl-3xl border-teal-950 mr-2 bg-[#F9F5E7]">
+            MY LOCAL TIME
           </div>
+
+          <Image
+            className="absolute ml-[85%]  "
+            src={bgImg4}
+            alt="My png Logo"
+            width={200}
+            height={400}
+          />
         </nav>
-        <h1 className="pt-24 pl-[40%] pb-7 font-sans font-extrabold text-[#0A1D37] text-[4rem] mr-72">
+
+        <h1 className="pt-16 pl-[40%] pb-7 font-sans font-extrabold text-[#0A1D37] text-[4rem] ">
           I am <span className="text-[#FF597B] font-mono">Arya</span>
         </h1>
-        <div id="h2" className="">
-          <h2 className="pt-0 mt-0 pl-[30%]  ml-64 font-mono font-extrabold text-[#0A1D37] text-4xl rotate-[12deg] ">
+        <div id="h2" className="p-0 m-0  ">
+          <h2 className="pt-0 mt-0 pl-[30%]  ml-64 font-mono font-extrabold text-[#0A1D37] text-4xl flex  ">
             A <span className="text-[#FF597B]">Future </span>Web-Developer
           </h2>
         </div>
 
-        <div className="flex flex-row mt-0">
+        <div className="flex flex-row mt-0 ">
           <div className="">
             <Image
               className=""
@@ -54,13 +67,13 @@ export default function Home() {
               height={700}
             />
           </div>
-          <h2 className="w-1/2 h-56 text-[#5F8670] m-0 p-0 text-3xl self-center pt-14 font-medium">
-            I Am a Civil Engineer and Interior Designer but At Least I have
-            decided to be a web developer
+          <h2 className="w-1/2  text-[#5F8670] m-0 p-0 text-[3.5rem] self-center pt-24 font-sans ">
+            Give someone a program; you frustrate them for a day; teach them how
+            to program, and you frustrate them for a lifetime, David Leinweber.
           </h2>
         </div>
       </div>
-      <hr className="bg-[#C3EDC0] w-[60%] h-2 border-solid border-stone-950 mb-20 self-start" />
+      <hr className=" w-[60%] h-2 border-solid border-stone-950 mb-20 self-start" />
       <div className="flex flex-row bg-yellow-100] pb-48">
         <div className="font-bold mb-24  text-3xl self-center pt-40  bg-yellow-100   ">
           <ul className="list-disc text-pretty  text-[#525E75] font-thin  ">
@@ -86,11 +99,11 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="align-center text-center text-4xl text-[#11A068] font-extrabold pb-8">
-        DO YOU NOW ME?
+      <div className="align-center text-center text-4xl text-[#11A068] font-extrabold pb-2">
+        DO YOU KNOW ME?
       </div>
       <div className="flex flex-row ">
-        <div className="flex flex-col flex-wrap  text-center pb-36">
+        <div className="flex flex-col flex-wrap  text-center ">
           <div className="border  border-teal-950 p-6 mt-44 rounded-lg ">
             I Love Dogs
           </div>
@@ -130,11 +143,11 @@ export default function Home() {
             width={600}
             height={600}
           />
-          <hr className="bg-[#C3EDC0] w-[100%] h-2 border-solid border-stone-950 " />
+          <hr className="bg-blac w-[100%] h-2 border-solid border-stone-950 " />
         </div>
       </div>
 
-      <div className="mt-32 bg-[#C3EDC0] w-[100%] pt-36 pb-8">
+      <div className="mt-40 bg-[#C3EDC0] w-[100%] pt-36 pb-8 ">
         <footer id="footer" className="font-normal">
           <section className="text-center">
             <a href="https://www.facebook.com/arya.messenger" target="_self">
